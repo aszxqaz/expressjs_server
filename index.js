76f738fd0976b675c786675b331d5323a7b7e9fe
Maxim B <b.maxim.dev@gmail.com>
2023-05-06 12:13:10 +0400

    fadasd

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import { Worker } from "worker_threads";
 import loop from "./loop.js";
 import express from "express";
 
+const NUMBER = 32;
+
 function setUpWorker() {
   return new Promise((res, rej) => {
     const worker = new Worker("./worker.js");
@@ -21,81 +23,37 @@ let asyncResult = 0;
 
 async function main() {
   let start = Date.now();
-  const asyncResult = await Promise.all([
-    setUpWorker(),
-    setUpWorker(),
-    setUpWorker(),
-    setUpWorker(),
-    setUpWorker(),
-    setUpWorker(),
-    setUpWorker(),
-    setUpWorker(),
-    setUpWorker(),
-    setUpWorker(),
-    setUpWorker(),
-    setUpWorker(),
-    setUpWorker(),
-    setUpWorker(),
-    setUpWorker(),
-    setUpWorker(),
-    setUpWorker(),
-    setUpWorker(),
-    setUpWorker(),
-    setUpWorker(),
-    setUpWorker(),
-    setUpWorker(),
-    setUpWorker(),
-    setUpWorker(),
-  ]).then((res) => res.reduce((a, b) => a + b));
+  const asyncResult = await Promise.all(
+    new Array(NUMBER).fill(0).map((_) => setUpWorker())
+  ).then((res) => res.reduce((a, b) => a + b));
   let end = Date.now();
   asyncPerf = end - start;
   console.log("async: ", asyncPerf);
   console.log("res: ", asyncResult);
 
   start = Date.now();
-  const syncResult =
-    loop() +
-    loop() +
-    loop() +
-    loop() +
-    loop() +
-    loop() +
-    loop() +
-    loop() +
-    loop() +
-    loop() +
-    loop() +
-    loop() +
-    loop() +
-    loop() +
-    loop() +
-    loop() +
-    loop() +
-    loop() +
-    loop() +
-    loop() +
-    loop() +
-    loop() +
-    loop() +
-    loop();
+  let syncResult =  0
+  for(let i = 0; i < NUMBER; i++) {
+    syncResult += loop()
+  }
   end = Date.now();
   syncPerf = end - start;
   console.log("sync: ", syncPerf);
   console.log("res: ", syncResult);
 
   const app = express();
-  
+
   app.get("/", (req, res) => {
     res.send(
       `asyncPerf: ${asyncPerf}; asyncRes: ${asyncResult}; syncPerf: ${syncPerf}; syncResult: ${syncResult}`
     );
   });
-  
+
   const port = process.env.PORT || 3000;
-  
+
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
   });
 }
 
-main()
+main();

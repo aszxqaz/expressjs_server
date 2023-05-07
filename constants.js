536301b73f1669fa59a05a1bb01104e0e715db27
Maxim B <b.maxim.dev@gmail.com>
2023-05-08 03:17:08 +0400

    fadasd

--- constants.js
+++ constants.js
@@ -4,4 +4,4 @@ export const WORDS = fs.readFileSync("words.txt").toString().split(" ");
 export const COUNT_TO_INFORM_PARENT = 5000;
 export const COUNT_TO_LOG_BY_PARENT = 500;
 export const FAKE_TEST_WALLET_COUNT = Infinity; 
-export const CHILDS_COUNT = 200
+export const CHILDS_COUNT = 100

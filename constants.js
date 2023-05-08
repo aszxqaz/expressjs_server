6b06c8e4cdf9e8de53449786e7886a19cdd2bbc9
Maxim B <b.maxim.dev@gmail.com>
2023-05-08 16:51:53 +0400

    fadasd

--- constants.js
+++ constants.js
@@ -1,7 +1,6 @@
 import * as fs from "fs";
 
 export const WORDS = fs.readFileSync("words.txt").toString().split(" ");
-export const COUNT_TO_INFORM_PARENT = 5000;
+export const COUNT_TO_INFORM_PARENT = 100;
 export const COUNT_TO_LOG_BY_PARENT = 500;
 export const FAKE_TEST_WALLET_COUNT = Infinity; 
-export const CHILDS_COUNT = 100
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import { exec, spawn } from "child_process";
 import {
-  CHILDS_COUNT,
   COUNT_TO_LOG_BY_PARENT,
   COUNT_TO_INFORM_PARENT,
   FAKE_TEST_WALLET_COUNT,
 } from "./constants.js";
+
+const index = process.argv.findIndex((_) => _.includes("count="));
+const string = process.argv[index];
+export const CHILDS_COUNT = 30;
+  // parseInt(string.slice(string.indexOf("=") + 1)) || 20;
 
 let walletsCount = 0;
 const startDate = Date.now();
@@ -16,11 +20,11 @@ let newMaximumCount = COUNT_TO_INFORM_PARENT;
 let countStep = COUNT_TO_INFORM_PARENT;
 let phraseBuiltTimes = 0;
 
-console.log("started");
+console.log(`App started at ${new Date().toLocaleString()}`);
 
 for (let i = 0; i < CHILDS_COUNT; i++) {
-  const a = exec("node woopy.js", {
-    // signal: ab.signal,
+  const a = exec("node woopy.js", {}).on("spawn", () => {
+    console.log(`Child process ${a.pid} started`);
   });
 
   a.stdout.on("data", (data) => {
@@ -36,21 +40,23 @@ for (let i = 0; i < CHILDS_COUNT; i++) {
     }
 
     if (walletsCount >= newMaximumCount) {
-      console.log(`${walletsCount.toLocaleString()} wallets scanned on Etherscan...`);
+      console.log(
+        `${walletsCount.toLocaleString()} wallets scanned on Etherscan...`
+      );
       console.log(`${phraseBuiltTimes.toLocaleString()} phrases built...`);
       console.log(`Instances up: ${CHILDS_COUNT}`);
-      
+
       const avgSpeed = (walletsCount / (Date.now() - startDate)) * 1000 * 60;
       const curSpeed =
-      ((walletsCount - lastCount) / (Date.now() - lastDate)) * 1000 * 60;
-      
+        ((walletsCount - lastCount) / (Date.now() - lastDate)) * 1000 * 60;
+
       lastCount = walletsCount;
       lastDate = Date.now();
-      
+
       // console.log(`Current speed: ${curSpeed.toFixed(2)} wallets / min.`);
       console.log(`Average speed: ${Math.trunc(avgSpeed)} wallets / min.`);
       console.log(`Wallets found: ${JSON.stringify(walletsFound)}`);
-      console.log('...');
+      console.log("...");
 
       newMaximumCount += countStep;
     }
